e71212bacdd5aeb9f27467c218deec5d59ba79a5
KosM <kosukemorimoto1@gmail.com>
2022-03-14 17:16:27 +0900

    Optimize build using Preact instead of React

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  ...withContentlayer()({ reactStrictMode: true }),
+  ...withContentlayer()({}),
   ...withBundleAnalyzer({}),
+  reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
 };
